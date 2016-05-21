a3cd81f7105721a00f1d3fcc964a1033e5d8a58a
Dawid Nowacki <dawinow271@student.polsl.pl>
2016-05-21 15:24:50 +0200

    Update Asistant Panel
    new boxes, program adds data to the table PossessSet, RecruitedSet and
    ApplicationSet

--- BD_Projekt/Forms/AsistantPanel.Designer.cs
+++ BD_Projekt/Forms/AsistantPanel.Designer.cs
@@ -39,7 +39,6 @@
             this.label11 = new System.Windows.Forms.Label();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.label14 = new System.Windows.Forms.Label();
-            this.textBox12 = new System.Windows.Forms.TextBox();
             this.label15 = new System.Windows.Forms.Label();
             this.textBox13 = new System.Windows.Forms.TextBox();
             this.label16 = new System.Windows.Forms.Label();
@@ -50,6 +49,11 @@
             this.button6 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -63,6 +67,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(80, 6);
             this.comboBox1.Name = "comboBox1";
@@ -105,7 +110,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(216, 9);
+            this.label10.Location = new System.Drawing.Point(211, 9);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(73, 13);
             this.label10.TabIndex = 18;
@@ -113,8 +118,9 @@
             // 
             // comboBox2
             // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(300, 6);
+            this.comboBox2.Location = new System.Drawing.Point(315, 6);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 19;
@@ -123,15 +129,15 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(216, 36);
+            this.label11.Location = new System.Drawing.Point(211, 36);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(49, 13);
+            this.label11.Size = new System.Drawing.Size(105, 13);
             this.label11.TabIndex = 20;
-            this.label11.Text = "Imię ojca";
+            this.label11.Text = "Doświadczenie (lata)";
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(300, 33);
+            this.textBox9.Location = new System.Drawing.Point(315, 33);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(121, 20);
             this.textBox9.TabIndex = 21;
@@ -139,23 +145,16 @@
             // label14
             // 
             this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(216, 62);
+            this.label14.Location = new System.Drawing.Point(211, 62);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(79, 13);
             this.label14.TabIndex = 26;
             this.label14.Text = "Data urodzenia";
             // 
-            // textBox12
-            // 
-            this.textBox12.Location = new System.Drawing.Point(300, 59);
-            this.textBox12.Name = "textBox12";
-            this.textBox12.Size = new System.Drawing.Size(121, 20);
-            this.textBox12.TabIndex = 27;
-            // 
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(216, 88);
+            this.label15.Location = new System.Drawing.Point(211, 88);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(64, 13);
             this.label15.TabIndex = 28;
@@ -163,7 +162,7 @@
             // 
             // textBox13
             // 
-            this.textBox13.Location = new System.Drawing.Point(300, 85);
+            this.textBox13.Location = new System.Drawing.Point(315, 85);
             this.textBox13.Name = "textBox13";
             this.textBox13.Size = new System.Drawing.Size(121, 20);
             this.textBox13.TabIndex = 29;
@@ -186,68 +185,116 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(15, 140);
+            this.button3.Location = new System.Drawing.Point(15, 203);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(406, 23);
+            this.button3.Size = new System.Drawing.Size(421, 23);
             this.button3.TabIndex = 34;
-            this.button3.Text = "Formularz umiejętności";
+            this.button3.Text = "Dodaj umiejętność";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.SkillsFormClick);
+            this.button3.Click += new System.EventHandler(this.skillsFormClick);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(15, 111);
+            this.button4.Location = new System.Drawing.Point(15, 234);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(186, 23);
+            this.button4.Size = new System.Drawing.Size(421, 23);
             this.button4.TabIndex = 35;
             this.button4.Text = "Załączanie dokumentów";
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.DocumentsAddClick);
+            this.button4.Click += new System.EventHandler(this.documentsAddClick);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(15, 169);
+            this.button5.Location = new System.Drawing.Point(15, 140);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(406, 23);
+            this.button5.Size = new System.Drawing.Size(421, 23);
             this.button5.TabIndex = 36;
             this.button5.Text = "Potwierdź informacje i dodaj do bazy";
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.ConfirmClick);
+            this.button5.Click += new System.EventHandler(this.confirmClick);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(15, 198);
+            this.button6.Location = new System.Drawing.Point(15, 263);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(406, 23);
+            this.button6.Size = new System.Drawing.Size(421, 23);
             this.button6.TabIndex = 37;
             this.button6.Text = "Wyczyść informacje";
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.ClearClick);
+            this.button6.Click += new System.EventHandler(this.clearClick);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(216, 116);
+            this.label3.Location = new System.Drawing.Point(211, 116);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(78, 13);
             this.label3.TabIndex = 38;
             this.label3.Text = "Wykształcenie\r\n";
- 
             // 
             // comboBox3
             // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(300, 111);
+            this.comboBox3.Location = new System.Drawing.Point(315, 113);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 39;
             this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.educationChoose);
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(316, 59);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(120, 20);
+            this.dateTimePicker1.TabIndex = 40;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "Umiejetności";
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(89, 174);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(227, 21);
+            this.comboBox4.TabIndex = 42;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(384, 174);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(52, 21);
+            this.comboBox5.TabIndex = 43;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(322, 177);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 44;
+            this.label6.Text = "Poziom";
+            // 
             // AsistantPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(436, 234);
+            this.ClientSize = new System.Drawing.Size(448, 298);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.comboBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.button6);
@@ -258,7 +305,6 @@
             this.Controls.Add(this.label16);
             this.Controls.Add(this.textBox13);
             this.Controls.Add(this.label15);
-            this.Controls.Add(this.textBox12);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.label11);
@@ -272,9 +318,11 @@
             this.Controls.Add(this.label1);
             this.Name = "AsistantPanel";
             this.Text = "Panel asystentki - rejestracja";
+            this.Load += new System.EventHandler(this.skillChoose);
             this.Load += new System.EventHandler(this.nationalityChoose);
-            this.Load += new System.EventHandler(this.positionChoose);
+            this.Load += new System.EventHandler(this.levelChoose);
             this.Load += new System.EventHandler(this.educationChoose);
+            this.Load += new System.EventHandler(this.positionChoose);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -293,7 +341,6 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.Label label14;
-        private System.Windows.Forms.TextBox textBox12;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox textBox13;
         private System.Windows.Forms.Label label16;
@@ -304,5 +351,10 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label6;
     }
 }
